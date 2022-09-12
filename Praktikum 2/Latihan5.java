c76780e0a6dc8eab871b8c1c5a897e4e8f185114
Fagih Akram <105544495+FagihAkram@users.noreply.github.com>
2022-09-12 11:59:30 +0700

    Create Latihan5.java

---
+++ Praktikum 2/Latihan5.java
@@ -0,0 +1,12 @@
+public class Latihan5 {
+    public static void main (String args[]) {
+        int b;
+
+        for (b = 1; b <= 20; b++){
+
+            if (b % 2 == 0 && b % 6 != 0) {
+                System.out.println(b);
+            }
+        }
+    }
+}
