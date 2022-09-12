e192a94c2e017bf5d3136f8350ad8ae1589e532a
Fagih Akram <105544495+FagihAkram@users.noreply.github.com>
2022-09-12 11:59:09 +0700

    Create Latihan4.java

---
+++ Praktikum 2/Latihan4.java
@@ -0,0 +1,11 @@
+public class Latihan4 {
+    public static void main (String args[]){
+        int a;
+
+        for (a = 1; a <= 20; a++){
+            if (a % 2 == 0) {
+                System.out.println(a);
+            }
+        }
+    }
+}
